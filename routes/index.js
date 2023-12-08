a9a5e36d0a1e9d07e580284dcb06e94ca0eaf12c
shivansh13579 <shivansh0975@gmail.com>
2023-12-08 06:35:08 +0530

    crate login and register page

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/register',function(req,res,next){
+  res.render('register')
+})
+
+module.exports = router;
